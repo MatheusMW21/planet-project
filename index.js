1f4bcbc45d03d37cf09f96c5e875a22286aad9d9
matheus <matheusmw21@gmail.com>
2024-02-09 07:38:26 -0300

    sortPlanets function

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const fs = require('fs');
 
 const habitablePlanets = [];
 
+//validate the habitable planets
 function isHabitablePlanet (planet) {
     return planet ['koi_disposition'] === 'CONFIRMED'
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
     && planet ['koi_prad'] < 1.6;
+}
+
+//sort the habitable planets from Radius
+function sortPlanets(planets, sortBy) {
+    return planets.sort((a, b) => {
+        if(a[sortBy] < b[sortBy]) return -1;
+        if(a[sortBy] > b[sortBy]) return 1;
+        return 0; 
+    })
 }
 
 fs.createReadStream('kepler_data.csv')
@@ -27,4 +37,12 @@ fs.createReadStream('kepler_data.csv')
             return planet['kepler_name'];
         }))
         console.log(`${habitablePlanets.length} habitable planets found!`)
+        const sortedByRadius = sortPlanets(habitablePlanets, 'koi_prad');
+
+        console.log("Habitable planets sorted by radius:");
+        sortedByRadius.forEach(planet => {
+            console.log(`${planet['kepler_name']} - Radius: ${planet['koi_prad']}`);
+        });
     });
+    
+
